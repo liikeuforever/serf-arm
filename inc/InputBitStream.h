fc0b35476558ee5d33b6666359c188bffc5faf68
thatcherchen <thatcherchen@163.com>
2024-01-18 23:27:04 +0800

    Save

--- inc/InputBitStream.h
+++ inc/InputBitStream.h
@@ -5,9 +5,31 @@
 #ifndef SERFNATIVE_INPUTBITSTREAM_H
 #define SERFNATIVE_INPUTBITSTREAM_H
 
+#include <fstream>
+#include <bitset>
 
 class InputBitStream {
+private:
+    std::ifstream &is_;
+    int bufCapacity_;
+    char *buffer_;
+    long bufCount_;
+    int bufPos_;
 
+public:
+    InputBitStream(std::ifstream &is, int bufCapacity) : is_(is), bufCapacity_(bufCapacity) {
+        buffer_ = new char[bufCapacity_];
+        bufCount_ = is_.readsome(buffer_, bufCapacity_);
+        bufPos_ = 0;
+    }
+
+    bool readBitTo(bool *dst) {
+        if ()
+    }
+
+    ~InputBitStream() {
+        delete buffer_;
+    }
 };
 
 
