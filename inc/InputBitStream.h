a6e90b4aad4a31deeccbe2d535f779cd11ddc2af
zcc0de <zechao.chen@kangry.net>
2024-01-19 15:48:03 +0800

    lightweight implementation of bit-level input stream

--- inc/InputBitStream.h
+++ inc/InputBitStream.h
@@ -1,6 +1,4 @@
-//
-// Created by czc on 18/01/24.
-//
+// Fast and lightweight implementation of bit-level input stream
 
 #ifndef SERFNATIVE_INPUTBITSTREAM_H
 #define SERFNATIVE_INPUTBITSTREAM_H
@@ -9,28 +7,68 @@
 #include <bitset>
 
 class InputBitStream {
+    InputBitStream(const char* data, size_t size) : data_(data), size_(size), index_(0), buffer_(0), bitsAvailable_(0) {}
+
+    // read `numBits` bits into an integer and return it
+    int readInt(size_t numBits) {
+        int result = 0;
+        while (numBits > 0) {
+            // Load more data into buffer when bits in buffer are less than `numBits`
+            if (bitsAvailable_ == 0) {
+                if (index_ < size_) {
+                    buffer_ = static_cast<unsigned char>(data_[index_]);
+                    bitsAvailable_ = 8;
+                    index_++;
+                } else {
+                    // Input data runs out
+                    break;
+                }
+            }
+
+            // read one bit from buffer
+            int bit = (buffer_ >> (bitsAvailable_ - 1)) & 1;
+            // add this bit to result
+            result = (result << 1) | bit;
+            // update self status
+            bitsAvailable_--;
+            numBits--;
+        }
+        return result;
+    }
+
+    // read `numBits` bits into a long and return it
+    long readLong(size_t numBits) {
+        int result = 0;
+        while (numBits > 0) {
+            // Load more data into buffer when bits in buffer are less than `numBits`
+            if (bitsAvailable_ == 0) {
+                if (index_ < size_) {
+                    buffer_ = static_cast<unsigned char>(data_[index_]);
+                    bitsAvailable_ = 8;
+                    index_++;
+                } else {
+                    // Input data runs out
+                    break;
+                }
+            }
+
+            // read one bit from buffer
+            int bit = (buffer_ >> (bitsAvailable_ - 1)) & 1;
+            // add this bit to result
+            result = (result << 1) | bit;
+            // update self status
+            bitsAvailable_--;
+            numBits--;
+        }
+        return result;
+    }
+
 private:
-    std::ifstream &is_;
-    int bufCapacity_;
-    char *buffer_;
-    long bufCount_;
-    int bufPos_;
-
-public:
-    InputBitStream(std::ifstream &is, int bufCapacity) : is_(is), bufCapacity_(bufCapacity) {
-        buffer_ = new char[bufCapacity_];
-        bufCount_ = is_.readsome(buffer_, bufCapacity_);
-        bufPos_ = 0;
-    }
-
-    bool readBitTo(bool *dst) {
-        if ()
-    }
-
-    ~InputBitStream() {
-        delete buffer_;
-    }
+    const char* data_;
+    size_t size_;
+    size_t index_;
+    unsigned char buffer_;
+    size_t bitsAvailable_;
 };
-
 
 #endif //SERFNATIVE_INPUTBITSTREAM_H
--- src/utils/InputBitStream.cpp
+++ src/utils/InputBitStream.cpp
@@ -1,5 +1,1 @@
-//
-// Created by czc on 18/01/24.
-//
-
 #include "InputBitStream.h"
