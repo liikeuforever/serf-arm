94aea6c4ed1b5b57c0eb4d85d753f091008e8fb8
zcc0de <zechao.chen@kangry.net>
2024-01-19 16:13:51 +0800

    lightweight implementation of bit-level output stream

--- inc/OutputBitStream.h
+++ inc/OutputBitStream.h
@@ -1,14 +1,62 @@
-//
-// Created by czc on 18/01/24.
-//
+// Fast and lightweight implementation of bit-level output stream
 
 #ifndef SERFNATIVE_OUTPUTBITSTREAM_H
 #define SERFNATIVE_OUTPUTBITSTREAM_H
 
+#include <vector>
 
 class OutputBitStream {
+public:
+    OutputBitStream() : buffer_(0), bitsAvailable_(8) {}
 
+    void writeInt(int data, size_t numBits) {
+        while (numBits > 0) {
+            buffer_ = (buffer_ << 1) | ((data >> (numBits - 1)) & 1);
+            bitsAvailable_--;
+            numBits--;
+
+            if (bitsAvailable_ == 0) {
+                flush();
+            }
+        }
+    }
+
+    void writeBit(bool bit) {
+        buffer_ = (buffer_ << 1) | (bit & 1);
+        bitsAvailable_--;
+
+        if (bitsAvailable_ == 0) {
+            flush();
+        }
+    }
+
+    void writeLong(long data, size_t numBits) {
+        while (numBits > 0) {
+            buffer_ = (buffer_ << 1) | ((data >> (numBits - 1)) & 1);
+            bitsAvailable_--;
+            numBits--;
+
+            if (bitsAvailable_ == 0) {
+                flush();
+            }
+        }
+    }
+
+    std::vector<char> getBuffer() {
+        flush();
+        return output_;
+    }
+
+    void flush() {
+        output_.push_back(static_cast<char>(buffer_));
+        buffer_ = 0;
+        bitsAvailable_ = 8;
+    }
+
+private:
+    unsigned char buffer_;
+    size_t bitsAvailable_;
+    std::vector<char> output_;
 };
-
 
 #endif //SERFNATIVE_OUTPUTBITSTREAM_H
