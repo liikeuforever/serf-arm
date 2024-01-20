b46be43e5bc1fba4cc19d3895da3e1c94718bdec
zcc0de <zechao.chen@kangry.net>
2024-01-20 17:11:09 +0800

    Refactor

--- inc/OutputBitStream.h
+++ inc/OutputBitStream.h
@@ -8,52 +8,11 @@
 class OutputBitStream {
 public:
     OutputBitStream() : buffer_(0), bitsAvailable_(8) {}
-
-    void writeInt(int data, size_t numBits) {
-        while (numBits > 0) {
-            buffer_ = (buffer_ << 1) | ((data >> (numBits - 1)) & 1);
-            bitsAvailable_--;
-            numBits--;
-
-            if (bitsAvailable_ == 0) {
-                flush();
-            }
-        }
-    }
-
-    int writeBit(bool bit) {
-        buffer_ = (buffer_ << 1) | (bit & 1);
-        bitsAvailable_--;
-
-        if (bitsAvailable_ == 0) {
-            flush();
-        }
-
-        return 1;
-    }
-
-    void writeLong(long data, size_t numBits) {
-        while (numBits > 0) {
-            buffer_ = (buffer_ << 1) | ((data >> (numBits - 1)) & 1);
-            bitsAvailable_--;
-            numBits--;
-
-            if (bitsAvailable_ == 0) {
-                flush();
-            }
-        }
-    }
-
-    std::vector<char> getBuffer() {
-        flush();
-        return output_;
-    }
-
-    void flush() {
-        output_.push_back(static_cast<char>(buffer_));
-        buffer_ = 0;
-        bitsAvailable_ = 8;
-    }
+    void writeInt(int data, size_t numBits);
+    int writeBit(bool bit);
+    void writeLong(long data, size_t numBits);
+    std::vector<char> getBuffer();
+    void flush();
 
 private:
     unsigned char buffer_;
--- src/utils/OutputBitStream.cpp
+++ src/utils/OutputBitStream.cpp
@@ -1,5 +1,48 @@
-//
-// Created by czc on 18/01/24.
-//
-
 #include "OutputBitStream.h"
+
+void OutputBitStream::writeInt(int data, size_t numBits) {
+    while (numBits > 0) {
+        buffer_ = (buffer_ << 1) | ((data >> (numBits - 1)) & 1);
+        bitsAvailable_--;
+        numBits--;
+
+        if (bitsAvailable_ == 0) {
+            flush();
+        }
+    }
+}
+
+int OutputBitStream::writeBit(bool bit) {
+    buffer_ = (buffer_ << 1) | (bit & 1);
+    bitsAvailable_--;
+
+    if (bitsAvailable_ == 0) {
+        flush();
+    }
+
+    return 1;
+}
+
+void OutputBitStream::writeLong(long data, size_t numBits) {
+    while (numBits > 0) {
+        buffer_ = (buffer_ << 1) | ((data >> (numBits - 1)) & 1);
+        bitsAvailable_--;
+        numBits--;
+
+        if (bitsAvailable_ == 0) {
+            flush();
+        }
+    }
+}
+
+std::vector<char> OutputBitStream::getBuffer() {
+    flush();
+    return output_;
+}
+
+void OutputBitStream::flush() {
+    output_.push_back(static_cast<char>(buffer_));
+    buffer_ = 0;
+    bitsAvailable_ = 8;
+}
+
