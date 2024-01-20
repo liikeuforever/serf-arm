a2ab34846c59accf4ff07ddf18e0caccfbfb38f0
zcc0de <zechao.chen@kangry.net>
2024-01-20 17:08:59 +0800

    Refactor

--- inc/InputBitStream.h
+++ inc/InputBitStream.h
@@ -4,73 +4,17 @@
 #define SERFNATIVE_INPUTBITSTREAM_H
 
 #include <fstream>
-#include <bitset>
 
 class InputBitStream {
-
 public:
-// read `numBits` bits into an integer and return it
-int readInt(size_t numBits) {
-    int result = 0;
-    while (numBits > 0) {
-        // Load more data into buffer when bits in buffer are less than `numBits`
-        if (bitsAvailable_ == 0) {
-            if (index_ < size_) {
-                buffer_ = static_cast<unsigned char>(data_[index_]);
-                bitsAvailable_ = 8;
-                index_++;
-            } else {
-                // Input data runs out
-                break;
-            }
-        }
-
-        // read one bit from buffer
-        int bit = (buffer_ >> (bitsAvailable_ - 1)) & 1;
-        // add this bit to result
-        result = (result << 1) | bit;
-        // update self status
-        bitsAvailable_--;
-        numBits--;
-    }
-    return result;
-}
-
-    InputBitStream(const char* data, size_t size) : data_(data), size_(size), index_(0), buffer_(0), bitsAvailable_(0) {}
-
-// read `numBits` bits into a long and return it
-long readLong(size_t numBits) {
-    int result = 0;
-    while (numBits > 0) {
-        // Load more data into buffer when bits in buffer are less than `numBits`
-        if (bitsAvailable_ == 0) {
-            if (index_ < size_) {
-                buffer_ = static_cast<unsigned char>(data_[index_]);
-                bitsAvailable_ = 8;
-                index_++;
-            } else {
-                // Input data runs out
-                break;
-            }
-        }
-
-        // read one bit from buffer
-        int bit = (buffer_ >> (bitsAvailable_ - 1)) & 1;
-        // add this bit to result
-        result = (result << 1) | bit;
-        // update self status
-        bitsAvailable_--;
-        numBits--;
-    }
-    return result;
-}
-
-int readBit() {
-    return readInt(1);
-}
+    InputBitStream(const char *data, size_t size) : data_(data), size_(size), index_(0), buffer_(0),
+                                                    bitsAvailable_(0) {}
+    int readInt(size_t numBits);
+    long readLong(size_t numBits);
+    int readBit();
 
 private:
-    const char* data_;
+    const char *data_;
     size_t size_;
     size_t index_;
     unsigned char buffer_;
--- src/utils/InputBitStream.cpp
+++ src/utils/InputBitStream.cpp
@@ -1,1 +1,57 @@
 #include "InputBitStream.h"
+
+int InputBitStream::readInt(size_t numBits) {
+    int result = 0;
+    while (numBits > 0) {
+        // Load more data into buffer when bits in buffer are less than `numBits`
+        if (bitsAvailable_ == 0) {
+            if (index_ < size_) {
+                buffer_ = static_cast<unsigned char>(data_[index_]);
+                bitsAvailable_ = 8;
+                index_++;
+            } else {
+                // Input data runs out
+                break;
+            }
+        }
+
+        // read one bit from buffer
+        int bit = (buffer_ >> (bitsAvailable_ - 1)) & 1;
+        // add this bit to result
+        result = (result << 1) | bit;
+        // update self status
+        bitsAvailable_--;
+        numBits--;
+    }
+    return result;
+}
+
+long InputBitStream::readLong(size_t numBits) {
+    int result = 0;
+    while (numBits > 0) {
+        // Load more data into buffer when bits in buffer are less than `numBits`
+        if (bitsAvailable_ == 0) {
+            if (index_ < size_) {
+                buffer_ = static_cast<unsigned char>(data_[index_]);
+                bitsAvailable_ = 8;
+                index_++;
+            } else {
+                // Input data runs out
+                break;
+            }
+        }
+
+        // read one bit from buffer
+        int bit = (buffer_ >> (bitsAvailable_ - 1)) & 1;
+        // add this bit to result
+        result = (result << 1) | bit;
+        // update self status
+        bitsAvailable_--;
+        numBits--;
+    }
+    return result;
+}
+
+int InputBitStream::readBit() {
+    return readInt(1);
+}
