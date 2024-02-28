1b60db404039f1b378ee3e6586e7c45408b84358
zcc0de <zechao.chen@kangry.net>
2024-02-28 12:04:01 +0800

    Add helper.sh

---
+++ helper.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ $1 == "build" ]]; then
+	cmake -B build && cmake --build build
+elif [[ $1 == "benchmark" ]]; then
+	printf "%s\n" "[WIP] Run benchmark"
+elif [[ $1 == "test" ]]; then
+	./build/bin/serf_test
+elif [[ $1 == "clean" ]]; then
+	if [[ -e build ]]; then
+		rm -r ./Build
+	fi
+else
+	printf "%s\n" "Serf Build Helper for everyone"
+	printf "%s\n" "Usage: "
+	printf "%s\n" "    helper.sh build - create build dir and build serf."
+	printf "%s\n" "    helper.sh benchmark - run benchmark."
+	printf "%s\n" "    helper.sh test - run all unit test."
+	printf "%s\n" "    helper.sh clean - remove build dir and clean all things."
+fi
