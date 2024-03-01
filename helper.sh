058f5d65074e66cb74b25192debe2b63b133a34a
zcc0de <zechao.chen@kangry.net>
2024-03-01 10:09:46 +0800

    Update helper.sh

--- helper.sh
+++ helper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $1 == "build" ]]; then
-	cmake -B build && cmake --build build
+	cmake -DCMAKE_BUILD_TYPE=$2 -B build && cmake --build build
 elif [[ $1 == "benchmark" ]]; then
 	printf "%s\n" "[WIP] Run benchmark"
 elif [[ $1 == "test" ]]; then
@@ -13,7 +13,7 @@ elif [[ $1 == "clean" ]]; then
 else
 	printf "%s\n" "Serf Build Helper for everyone"
 	printf "%s\n" "Usage: "
-	printf "%s\n" "    helper.sh build - create build dir and build serf."
+	printf "%s\n" "    helper.sh build [release/debug] - create build dir and build serf."
 	printf "%s\n" "    helper.sh benchmark - run benchmark."
 	printf "%s\n" "    helper.sh test - run all unit test."
 	printf "%s\n" "    helper.sh clean - remove build dir and clean all things."
