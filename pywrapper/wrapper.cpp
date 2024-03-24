e750631eb7e4d1622db66dafb8a6c596c235c001
zcc0de <zechao.chen@kangry.net>
2024-03-24 19:33:57 +0800

    Update pywrapper

--- pywrapper/wrapper.cpp
+++ pywrapper/wrapper.cpp
@@ -4,6 +4,8 @@
 
 #include "serf/compressor/SerfXORCompressor.h"
 #include "serf/decompressor/SerfXORDecompressor.h"
+#include "serf/compressor/TorchSerfXORCompressor.h"
+#include "serf/decompressor/TorchSerfXORDecompressor.h"
 
 namespace py = pybind11;
 
@@ -28,6 +30,27 @@ PYBIND11_MODULE(pyserf, m) {
     py::class_<SerfXORDecompressor>(m, "SerfXORDecompressor")
             .def(py::init<long>())
             .def("decompress", [](SerfXORDecompressor &xorDecompressor, std::vector<uint8_t> compressedPack) {
+                Array<uint8_t> input = Array<uint8_t>(compressedPack.size());
+                for (int i = 0; i < compressedPack.size(); ++i) {
+                    input[i] = compressedPack[i];
+                }
+                return xorDecompressor.decompress(input);
+            });
+
+    py::class_<TorchSerfXORCompressor>(m, "TorchSerfXORCompressor")
+            .def(py::init<double, long>())
+            .def("compress", [](TorchSerfXORCompressor &xorCompressor, double v) {
+                Array<uint8_t> compressPack = xorCompressor.compress(v);
+                std::vector<uint8_t> bin;
+                for (int i = 0; i < compressPack.length; ++i) {
+                    bin.emplace_back(compressPack[i]);
+                }
+                return bin;
+            });
+
+    py::class_<TorchSerfXORDecompressor>(m, "TorchSerfXORDecompressor")
+            .def(py::init<long>())
+            .def("decompress", [](TorchSerfXORDecompressor &xorDecompressor, std::vector<uint8_t> compressedPack) {
                 Array<uint8_t> input = Array<uint8_t>(compressedPack.size());
                 for (int i = 0; i < compressedPack.size(); ++i) {
                     input[i] = compressedPack[i];
