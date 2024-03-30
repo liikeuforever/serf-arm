9ac1df8214e9e5d7a60981cb2b943a5b14a65fb4
zcc0de <zechao.chen@kangry.net>
2024-03-30 14:13:08 +0800

    Remove deprecated code

--- src/serf/concurrency/SerfTransaction.cpp
+++ src/serf/concurrency/SerfTransaction.cpp
@@ -9,20 +9,6 @@ SerfTransaction::SerfTransaction(int d1, int d2, int d3, int d4): compressor_arr
     omp_set_num_threads(static_cast<int>(std::thread::hardware_concurrency()) * 2);
 }
 
-//std::vector<std::vector<uint8_t>> SerfTransaction::compress_tensor(std::vector<double> flat_tensor) {
-//    std::vector<std::vector<uint8_t>> result(flat_tensor.size());
-//    #pragma omp parallel for
-//    for (int i = 0; i < compressor_array_len; ++i) {
-//        Array<uint8_t> compressed = compressor_array[i]->compress(flat_tensor[i]);
-//        std::vector<uint8_t> compressed_v(compressed.length);
-//        for (int j = 0; j < compressed.length; ++j) {
-//            compressed_v.push_back(compressed[j]);
-//        }
-//        result[i] = compressed_v;
-//    }
-//    return result;
-//}
-
 std::vector<std::vector<uint8_t>> SerfTransaction::compress_tensor(std::vector<double> flat_tensor) {
     std::vector<std::vector<uint8_t>> result(flat_tensor.size());
     #pragma omp parallel for
