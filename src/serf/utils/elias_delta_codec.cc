90b2de1e1e0da2a01e097a5731de5da1bdea2205
zcc0de <zechao.chen@kangry.net>
2024-05-20 15:12:49 +0800

    refactor elias_delta_codec.cc

--- src/serf/utils/elias_delta_codec.cc
+++ src/serf/utils/elias_delta_codec.cc
@@ -1,28 +1,40 @@
-#include "elias_delta_codec.h"
+#include "serf/utils/elias_delta_codec.h"
 
-int elias_delta_codec::Encode(int64_t number, OutputBitStream *output_bit_stream_ptr) {
+int elias_delta_codec::Encode(int64_t number,
+                              OutputBitStream *output_bit_stream_ptr) {
     int compressed_size_in_bits = 0;
     int32_t len;
     int32_t length_of_len;
     if (number <= 11) {
-        len = 1 + (int) std::floor(kLogTable[number] / kLogTable[2]);
+        len = 1 + static_cast<int>(std::floor(kLogTable[number] /
+                                              kLogTable[2]));
     } else {
-        len = 1 + (int) std::floor(std::log(number) / kLogTable[2]);
+        len = 1 + static_cast<int>(std::floor(std::log(number) / kLogTable[2]));
     }
     if (len <= 11) {
-        length_of_len = (int) std::floor(kLogTable[len] / kLogTable[2]);
+        length_of_len = static_cast<int>(std::floor(
+                kLogTable[len] / kLogTable[2]));
     } else {
-        length_of_len = (int) std::floor(std::log(len) / kLogTable[2]);
+        length_of_len = static_cast<int>(std::floor(
+                std::log(len) / kLogTable[2]));
     }
     int total_len = length_of_len + length_of_len + len;
     if (total_len <= 64) {
-        compressed_size_in_bits += static_cast<int>(output_bit_stream_ptr->writeLong(((int64_t) len << (len - 1)) |
-                                                                                     (number & ~(0xffffffffffffffffL << (len - 1))),
-                                                                                     total_len));
+        compressed_size_in_bits += static_cast<int>(
+                output_bit_stream_ptr->writeLong(
+                        ((int64_t) len << (len - 1)) |
+                        (number & ~(0xffffffffffffffffL << (len - 1))),
+                        total_len));
     } else {
-        compressed_size_in_bits += static_cast<int>(output_bit_stream_ptr->writeInt(0, length_of_len));
-        compressed_size_in_bits += static_cast<int>(output_bit_stream_ptr->writeInt(len, length_of_len + 1));
-        compressed_size_in_bits += static_cast<int>(output_bit_stream_ptr->writeLong(number, len - 1));
+        compressed_size_in_bits += static_cast<int>(
+                output_bit_stream_ptr->writeInt(
+                        0, length_of_len));
+        compressed_size_in_bits += static_cast<int>(
+                output_bit_stream_ptr->writeInt(
+                        len, length_of_len + 1));
+        compressed_size_in_bits += static_cast<int>(
+                output_bit_stream_ptr->writeLong(
+                        number, len - 1));
     }
     return compressed_size_in_bits;
 }
